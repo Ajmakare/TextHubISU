2108846a12867af3bf55a8b3354691c90018a7d2
Aidan Makare <aidanmakaree@gmail.com>
2022-11-01 20:17:03 -0500

    initialized test case classes for what we have so far

--- texthubISU/texthubhomepage/tests.py
+++ texthubISU/texthubhomepage/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from texthubhomepage.migrations.DataStores import AdminDataStore, TextbookDataStore
+from texthubhomepage.migrations.Controllers import AdminController, SiteController, TextbookController
 
-# Create your tests here.
+
+class AdminDataStoreTest(TestCase):
+    # test cases
+    pass
+
+class TextbookDataStoreTest(TestCase):
+    pass
+
+class AdminControllerTest(TestCase):
+    pass
+
+class SiteControllerTest(TestCase):
+    pass
+
+class TextbookControllerTest(TestCase):
+    pass
+
