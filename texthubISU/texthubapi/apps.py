5bb2cbdf52f4f3adbb48e204c817386780a5a752
Aidan Makare <aidanmakaree@gmail.com>
2022-11-03 15:27:50 -0500

    Project naming changes

--- texthubISU/texthubapi/apps.py
+++ texthubISU/texthubapi/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TexthubhomepageConfig(AppConfig):
+class Texthubapi(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'texthubhomepage'
+    name = 'texthubapi'
--- texthubISU/texthubapi/views.py
+++ texthubISU/texthubapi/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework import viewsets
 
 from .serializers import *
-from texthubhomepage.Controllers.TextbookController import do_search_controller
+from texthubapi.Controllers.TextbookController import do_search_controller
 
 def index(request):
     return HttpResponse("Welcome to the ISU TextHub home page!")
