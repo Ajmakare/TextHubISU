199255aeb379868bb547df7906530dfbeb791351
Aidan Makare <aidanmakaree@gmail.com>
2022-11-26 15:13:32 -0600

    Submit review test case

--- texthubISU/texthubapi/tests.py
+++ texthubISU/texthubapi/tests.py
@@ -1,13 +1,19 @@
-from django.test import TestCase
-from DataStores import AdminDataStore, TextbookDataStore
-from Controllers import AdminController, SiteController, TextbookController
+from django.test import TestCase,RequestFactory
+from texthubapi.Controllers.TextbookController import *
+from texthubapi.DataStores.TextbookDataStore import *
+from .models import *
+from http import HTTPStatus
 
-
-class AdminDataStoreTest(TestCase):
-    # test cases
+class AdminTest(TestCase):
     pass
 
-class TextbookDataStoreTest(TestCase):
-    pass
+class TextbookTest(TestCase):
+    @classmethod
+    def setUp(self):
+        # Textbook object for review object (isbn is foreign key for Review object)
+        self.textbook = Textbook.objects.create(ISBN = 'testisbn', author = 'Aidan', name = 'how to code', view_count = 0)
 
+    def test_submit_review_pass(self):
+        response = self.client.post('/home', data = {'ISBNToReview': 'testisbn','ReviewContent':'12020424' })
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
