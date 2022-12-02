48da2ff8cb5f3aabc82669e49b01f135d27a3405
Aidan Makare <92825395+Ajmakare@users.noreply.github.com>
2022-12-02 01:09:08 +0000

    url changes

--- texthubISU/texthubapi/urls.py
+++ texthubISU/texthubapi/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('textbooks/<ISBN>/<sort>', Views.DoSearchView.as_view()),
     path('', include('rest_framework.urls', namespace='rest_framework')),
     path('admin2', Views.admin, name="admin"),
-    # path('login2', views.login, name = "login"),
+    path('login2/', Views.login_view, name = "login2"),
     path('home', Views.home_view, name='home'),
     path('sendrequest/', Views.sendRequest_view, name = "sendrequest"),
     path('retrieve', Views.RetrieveView.as_view())
