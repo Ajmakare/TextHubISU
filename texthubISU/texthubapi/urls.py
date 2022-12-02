113a86021e146073ae4580d0c5daf5c58ef59864
cdyoun2 <firefast17@gmail.com>
2022-12-01 19:14:43 -0600

    fixed url problems

--- texthubISU/texthubapi/urls.py
+++ texthubISU/texthubapi/urls.py
@@ -5,9 +5,9 @@ from .views import Views
 urlpatterns = [
     path('textbooks/<ISBN>/<sort>', Views.DoSearchView.as_view()),
     path('', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin2', Views.admin, name="admin"),
-    path('login2/', Views.login_view, name = "login2"),
+    path('admin2', Views.admin, name="admin2"),
+    path('login2/', Views.login_view, name="login2"),
     path('home', Views.home_view, name='home'),
-    path('sendrequest/', Views.sendRequest_view, name = "sendrequest"),
+    path('sendrequest/', Views.sendRequest_view, name="sendrequest"),
     path('retrieve', Views.RetrieveView.as_view())
 ]
